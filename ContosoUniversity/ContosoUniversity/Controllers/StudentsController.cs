3e7e6ea67341fc4d6a7b9e4b3403f5e1d852dd5e
Thato <t.chitja369@gmail.com>
2023-10-09 02:57:20 +0200

    Update Delete function to read-first approach

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -137,18 +137,27 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
-            if (id == null || _context.Students == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
             var student = await _context.Students
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
+
             if (student == null)
             {
                 return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] = 
+                    "Delete failed. Try again, and if the problem persists " + 
+                    "see your system administrator.";
             }
 
             return View(student);
@@ -159,18 +168,23 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Students == null)
-            {
-                return Problem("Entity set 'SchoolContext.Students'  is null.");
-            }
             var student = await _context.Students.FindAsync(id);
-            if (student != null)
+
+            if (student == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
             {
                 _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            } catch (DbUpdateException ex)
+            {
+                // Log the error
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool StudentExists(int id)
